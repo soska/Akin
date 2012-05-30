17f29fac0634074205b79ecc77d9fc616c276899
Armando Sosa <asosa@nolimit-studio.com>
2012-05-30 01:27:23 -0500

    Fixed bugs, added pinterest service

--- src/services.js
+++ src/services.js
@@ -5,8 +5,12 @@
 // Simple share by email.
 Akin.fn.email = function(){
   // email should open in same window no matter what.
+  var subject = this.settings.email.subject || this.settings.subject || this.settings.title;
+  var body = this.settings.email.body || this.settings.body || subject;
+  var url = this.settings.email.url || this.settings.url;
+
   this.settings.same_window = true;
-  this.share_url = "mailto:?subject=" + this.settings.title + "&body="  + this.settings.title + "  " + this.settings.url;
+  this.share_url = "mailto:?subject=" + subject + "&body="  + body + "  " + url;
 };
 
 
@@ -14,7 +18,10 @@ Akin.fn.email = function(){
 // Share by twitter.
 Akin.fn.twitter = function(){
 
-  this.share_url = "http://twitter.com/share?text=" + this.settings.title + "&url=" + this.settings.url; 
+  var url = this.settings.twitter.url || this.settings.url;
+  var tweet = this.settings.twitter.tweet || this.settings.tweet || this.settings.title;
+
+  this.share_url = "http://twitter.com/share?text=" + tweet + "&url=" + url; 
 
   if ( this.settings.twitter ) {
     var o = this.settings.twitter;
@@ -34,5 +41,18 @@ Akin.fn.twitter = function(){
 // Share by facebook
 // TODO : Use a facebook dialog if credentials are available.
 Akin.fn.facebook = function(){
-  this.share_url = "http://facebook.com/sharer.php?t=" + this.settings.title + "&u=" + this.settings.url;
+  var caption = this.settings.caption || this.settings.title;
+  this.share_url = "http://facebook.com/sharer.php?t=" + caption + "&u=" + this.settings.url;
 };
+
+// ## Pinterest
+// Share by facebook
+// TODO : Use a facebook dialog if credentials are available.
+Akin.fn.pinterest = function(){
+  var description = this.settings.description || this.settings.title;
+  var media = this.settings.pinterest.media || this.settings.media;
+  var url = this.settings.pinterest.url || this.settings.url;
+
+  this.share_url = "http://pinterest.com/pin/create/bookmarklet/?media="+media+"&url="+url+"&description="+description;
+};
+
